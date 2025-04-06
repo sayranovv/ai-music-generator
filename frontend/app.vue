898c33bb5e992ae65a495f6aff0bc6b0d636410f
sayranovv <sayranoveldar@gmail.com>
2025-04-06 19:17:05 +0500

    Created layout & index page

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <div
+      class="sm:flex sm:justify-between sm:items-center sm:px-10 lg:px-24 2xl:justify-center 2xl:gap-20 sm:h-screen sm:bg-gradient-to-br sm:from-primary-50 sm:to-primary-500 dark:sm:from-primary-400 dark:sm:to-primary-800"
+    >
+      <div class="hidden sm:block w-1/2">
+        <h1
+          class="text-primary-500 dark:text-primary-950 sm:text-4xl lg:text-6xl sm:mb-32 2xl:pl-20"
+        >
+          <span class="font-bold">AI music generator</span> <br />
+        </h1>
+      </div>
+      <div class="sm:w-1/2 sm:pr-10">
+        <NuxtLayout class="sm:rounded-3xl sm:shadow-2xl sm:mx-auto overflow-hidden">
+          <NuxtPage />
+        </NuxtLayout>
+      </div>
+    </div>
+  </UApp>
 </template>
+<script setup lang="ts"></script>
