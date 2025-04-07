7c00eade8a15dfcaa3ebdc0bebc7635a69314ff0
sayranovv <sayranoveldar@gmail.com>
2025-04-07 20:44:30 +0500

    Create backend with FastAPI

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+from typing import Union
+from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse
+import torch
+from audiocraft.models import MusicGen
+import torchaudio
+import uuid
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+model = MusicGen.get_pretrained('facebook/musicgen-small')
+model.set_generation_params(duration=5)
+
+@app.post("/generate")
+def generate_music(prompt: str = Form(...)):
+    wav = model.generate([prompt])
+    wav_tensor = wav[0].cpu()
+    sample_rate = model.sample_rate
+
+    filename = f"generated_{uuid.uuid4().hex[:8]}.wav"
+    output_path = os.path.join("generated", filename)
+    os.makedirs("generated", exist_ok=True)
+    torchaudio.save(output_path, wav_tensor, sample_rate)
+
+    return FileResponse(output_path, media_type="audio/wav", filename=filename)
