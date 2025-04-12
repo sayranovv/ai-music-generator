2e018276880113fb58a1dec54f83d9cd7880fefc
sayranovv <sayranoveldar@gmail.com>
2025-04-12 22:40:41 +0500

    Return to old logic

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useGenerateMusic } from '~/composables/useGenerateMusic'
+import { useMainStore } from '~/stores/main'
 
-const artistInput = ref('')
-const { isLoading, error, getMusicByArtist, generatedMusic } = useGenerateMusic()
+const store = useMainStore()
 
-const onGenerateMusic = () => {
-  if (artistInput.value) {
-    getMusicByArtist(artistInput.value)
-  }
+const artist = ref('')
+const result = ref()
+
+const searchArtist = async () => {
+  const { data } = await useFetch('/api/spotify', {
+    method: 'POST',
+    body: { artist: artist.value },
+  })
+  result.value = data.value
+  console.log(result.value)
 }
 </script>
 
@@ -18,40 +22,20 @@ const onGenerateMusic = () => {
       <h1 class="text-center text-3xl text-primary-500 dark:text-primary-400 font-bold">
         Generate music by artist
       </h1>
-      <UInput size="xl" highlight class="w-full" v-model="artistInput" />
-
-      <!-- Кнопка генерации музыки -->
+      <UInput size="xl" highlight class="w-full" v-model="artist" />
       <UButton
-        size="xl"
-        class="w-full flex items-center justify-center"
-        :disabled="isLoading"
-        @click="onGenerateMusic"
+          size="xl"
+          class="w-full flex items-center justify-center"
+          @click="store.searchArtist(artist)"
+      >Generate music</UButton
       >
-        Generate music
-      </UButton>
+    </div>
+    <div v-if="result" class="mt-4">
+      <pre>{{ result?.artists?.items[0] }}</pre>
+    </div>
 
-      <!-- Индикация загрузки -->
-      <div v-if="isLoading" class="text-center text-gray-500 mt-4">
-        Generating music... Please wait.
-      </div>
-
-      <!-- Ошибка -->
-      <div v-if="error" class="text-red-500 mt-4">{{ error }}</div>
-
-      <!-- Отображение сгенерированного аудио файла -->
-      <div v-if="generatedMusic" class="mt-4">
-        <h2 class="text-center text-2xl text-primary-500 dark:text-primary-400 font-semibold">
-          Your Generated Music
-        </h2>
-        <audio controls class="w-full mt-2">
-          <source :src="generatedMusic.url" type="audio/wav" />
-          Your browser does not support the audio element.
-        </audio>
-      </div>
+    <div class="mt-4">
+      <pre class="text-blue-500">{{ store.artistData }}</pre>
     </div>
   </div>
 </template>
-
-<style scoped>
-/* Можете добавить свои стили */
-</style>
