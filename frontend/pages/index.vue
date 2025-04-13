7611744bc0b847ddaa08bb5d21d13605e42851d4
Eldar Sayranov <sayranoveldar@gmail.com>
2025-04-13 22:07:22 +0500

    Edit logic

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,15 +4,10 @@ import { useMainStore } from '~/stores/main'
 const store = useMainStore()
 
 const artist = ref('')
-const result = ref()
 
-const searchArtist = async () => {
-  const { data } = await useFetch('/api/spotify', {
-    method: 'POST',
-    body: { artist: artist.value },
-  })
-  result.value = data.value
-  console.log(result.value)
+const onClick = () => {
+  store.setArtistName(artist.value)
+  store.searchArtist()
 }
 </script>
 
@@ -23,17 +18,10 @@ const searchArtist = async () => {
         Generate music by artist
       </h1>
       <UInput size="xl" highlight class="w-full" v-model="artist" />
-      <UButton
-          size="xl"
-          class="w-full flex items-center justify-center"
-          @click="store.searchArtist(artist)"
-      >Generate music</UButton
+      <UButton size="xl" class="w-full flex items-center justify-center" @click="onClick"
+        >Generate music</UButton
       >
     </div>
-    <div v-if="result" class="mt-4">
-      <pre>{{ result?.artists?.items[0] }}</pre>
-    </div>
-
     <div class="mt-4">
       <pre class="text-blue-500">{{ store.artistData }}</pre>
     </div>
