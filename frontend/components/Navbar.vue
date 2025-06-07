40f8af04df966d2ba46fa7ed34a7fac54d298855
sayranovv <sayranoveldar@gmail.com>
2025-06-07 17:44:42 +0500

    Create navbar

---
+++ frontend/components/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div
+    class="fixed sm:absolute bottom-0 right-0 flex justify-between bg-white/40 dark:bg-black/40 backdrop-blur-2xl w-full px-16 py-5"
+  >
+    <NuxtLink to="/" class="flex flex-col items-center gap-1">
+      <UIcon name="material-symbols:home-rounded" size="24" />
+    </NuxtLink>
+    <NuxtLink to="/trips">
+      <UIcon name="material-symbols:home-rounded" class="" size="24" />
+    </NuxtLink>
+    <NuxtLink to="/profile">
+      <UIcon name="material-symbols:home-rounded" class="" size="24" />
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped></style>
