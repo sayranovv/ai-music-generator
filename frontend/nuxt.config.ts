f20a289cae0dfcdadc4b1e56f7b6529e1a76c455
sayranovv <sayranoveldar@gmail.com>
2025-06-07 17:43:25 +0500

    Add appwrite

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { process } from 'std-env'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/ui', '@pinia/nuxt'],
   css: ['~/assets/css/main.css'],
+  runtimeConfig: {
+    public: {
+      appwriteEndpoint: process.env.NUXT_PUBLIC_APPWRITE_ENDPOINT,
+      appwriteProjectId: process.env.NUXT_PUBLIC_APPWRITE_PROJECT_ID,
+      appwriteProjectName: process.env.NUXT_PUBLIC_APPWRITE_PROJECT_NAME,
+    },
+  },
 })
