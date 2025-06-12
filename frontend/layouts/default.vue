2c77b21a703f46409a5678d0be693cf0bf396f64
sayranovv <sayranoveldar@gmail.com>
2025-06-12 16:51:36 +0500

    Remove paddings from layout

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="relative bg-white dark:bg-black sm:w-96 h-dvh sm:h-[700px] flex flex-col">
-    <div class="flex-1 overflow-y-auto pt-8 px-4 pb-4">
+    <div class="overflow-y-auto">
       <slot />
     </div>
     <Navbar />
