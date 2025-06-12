f9b727273bdef83ea06017e40321ad169eb3f548
sayranovv <sayranoveldar@gmail.com>
2025-06-12 18:43:40 +0500

    Add icons to navbar

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,17 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute()
+</script>
 
 <template>
   <div
-    class="fixed sm:absolute bottom-0 right-0 flex justify-between bg-white/20 dark:bg-rose-300/20 backdrop-blur-2xl w-full px-16 py-5"
+    class="fixed sm:absolute bottom-0 right-0 flex justify-between bg-white/20 dark:bg-rose-300/20 backdrop-blur-2xl w-full px-16 pt-2 pb-3 border-t border-t-rose-300/30"
   >
-    <NuxtLink to="/" class="flex flex-col items-center gap-1">
-      <UIcon name="material-symbols:home-rounded" size="24" />
+    <NuxtLink to="/" class="flex flex-col items-center justify-center gap-1 w-10">
+      <UIcon v-if="route.path === '/'" name="material-symbols:home-rounded" size="28" />
+      <UIcon
+        v-else
+        name="material-symbols:home-outline-rounded"
+        class="text-rose-100/60"
+        size="28"
+      />
+      <p class="font-medium text-xs" :class="{ 'text-rose-100/60': route.path !== '/' }">Home</p>
     </NuxtLink>
-    <NuxtLink to="/trips">
-      <UIcon name="material-symbols:home-rounded" class="" size="24" />
+    <NuxtLink to="/history" class="flex flex-col items-center justify-center gap-1 w-10">
+      <UIcon
+        name="material-symbols:history-rounded"
+        :class="{ 'text-rose-100/60': route.path !== '/history' }"
+        size="28"
+      />
+      <p class="font-medium text-xs" :class="{ 'text-rose-100/60': route.path !== '/history' }">
+        History
+      </p>
     </NuxtLink>
-    <NuxtLink to="/profile">
-      <UIcon name="material-symbols:home-rounded" class="" size="24" />
+    <NuxtLink to="/profile" class="flex flex-col items-center justify-center gap-1 w-10">
+      <UIcon v-if="route.path === '/profile'" name="material-symbols:person-rounded" size="28" />
+      <UIcon
+        v-else
+        name="material-symbols:person-outline-rounded"
+        class="text-rose-100/60"
+        size="28"
+      />
+      <p class="font-medium text-xs" :class="{ 'text-rose-100/60': route.path !== '/profile' }">
+        Profile
+      </p>
     </NuxtLink>
   </div>
 </template>
