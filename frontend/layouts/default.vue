ced911d91148ba47e6003ec68855f60e17a63961
sayranovv <sayranoveldar@gmail.com>
2025-06-13 23:07:01 +0500

    Fix layout

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="relative bg-white dark:bg-black sm:w-96 h-dvh sm:h-[700px] flex flex-col">
-    <div class="overflow-y-auto">
+    <div class="overflow-y-auto flex-1">
       <slot />
     </div>
     <Navbar />
