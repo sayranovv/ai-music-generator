28182cdb2cb817081fe982c95d4b25a95e540822
sayranovv <sayranoveldar@gmail.com>
2025-06-13 23:09:37 +0500

    Create empty layout for welcome page

---
+++ frontend/layouts/empty.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="relative bg-white dark:bg-black sm:w-96 h-dvh sm:h-[700px] flex flex-col">
+    <div class="overflow-y-auto flex-1">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
