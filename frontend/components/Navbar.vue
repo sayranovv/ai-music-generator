b84ca9b49d494ac17fb0f36c5b7b533df74e862b
sayranovv <sayranoveldar@gmail.com>
2025-06-13 23:08:35 +0500

    Fix navbar icons sizes

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -4,15 +4,15 @@ const route = useRoute()
 
 <template>
   <div
-    class="fixed sm:absolute bottom-0 right-0 flex justify-between bg-white/20 dark:bg-rose-300/20 backdrop-blur-2xl w-full px-16 pt-2 pb-3 border-t border-t-rose-300/30"
+    class="fixed sm:absolute bottom-0 right-0 flex justify-between bg-white/20 dark:bg-rose-300/14 backdrop-blur-2xl w-full px-16 pt-2 pb-4 border-t border-t-rose-300/30"
   >
     <NuxtLink to="/" class="flex flex-col items-center justify-center gap-1 w-10">
-      <UIcon v-if="route.path === '/'" name="material-symbols:home-rounded" size="28" />
+      <UIcon v-if="route.path === '/'" name="material-symbols:home-rounded" size="24" />
       <UIcon
         v-else
         name="material-symbols:home-outline-rounded"
         class="text-rose-100/60"
-        size="28"
+        size="24"
       />
       <p class="font-medium text-xs" :class="{ 'text-rose-100/60': route.path !== '/' }">Home</p>
     </NuxtLink>
@@ -20,19 +20,19 @@ const route = useRoute()
       <UIcon
         name="material-symbols:history-rounded"
         :class="{ 'text-rose-100/60': route.path !== '/history' }"
-        size="28"
+        size="24"
       />
       <p class="font-medium text-xs" :class="{ 'text-rose-100/60': route.path !== '/history' }">
         History
       </p>
     </NuxtLink>
     <NuxtLink to="/profile" class="flex flex-col items-center justify-center gap-1 w-10">
-      <UIcon v-if="route.path === '/profile'" name="material-symbols:person-rounded" size="28" />
+      <UIcon v-if="route.path === '/profile'" name="material-symbols:person-rounded" size="24" />
       <UIcon
         v-else
         name="material-symbols:person-outline-rounded"
         class="text-rose-100/60"
-        size="28"
+        size="24"
       />
       <p class="font-medium text-xs" :class="{ 'text-rose-100/60': route.path !== '/profile' }">
         Profile
