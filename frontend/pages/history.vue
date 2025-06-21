661ed4b8d53990337f9fff1ba651eb31fa879d7b
sayranovv <sayranoveldar@gmail.com>
2025-06-21 13:43:26 +0500

    Add loader & no tracks states

--- frontend/pages/history.vue
+++ frontend/pages/history.vue
@@ -3,9 +3,7 @@ import { useTracksStore } from '~/stores/tracks'
 
 const tracksStore = useTracksStore()
 
-onMounted(async () => {
-  await tracksStore.getTracks()
-})
+onMounted(async () => await tracksStore.getTracks())
 </script>
 
 <template>
@@ -14,17 +12,48 @@ onMounted(async () => {
       <h1 class="text-center text-lg font-bold">History</h1>
     </header>
     <main>
-      <Card
-        v-if="tracksStore.tracks"
-        v-for="track in tracksStore.tracks"
-        :genre="track.genres"
-        :description="track.description"
-        :name="track.name"
-        :src="track.audioUrl"
-        :img="track.imageUrl"
-      />
+      <Transition name="fade" v-if="tracksStore.tracks.length > 0">
+        <div>
+          <Card
+            v-if="tracksStore.tracks"
+            v-for="track in tracksStore.tracks"
+            :genre="track.genres"
+            :description="track.description"
+            :name="track.name"
+            :src="track.audioUrl"
+            :img="track.imageUrl"
+          />
+        </div>
+      </Transition>
+      <Transition name="fade" v-else-if="!tracksStore.isLoading">
+        <p class="text-center">No tracks</p>
+      </Transition>
     </main>
+    <Transition name="fade">
+      <Loader
+        v-if="tracksStore.isLoading"
+        class="absolute w-full flex"
+        :class="{
+          'top-7 right-10 justify-end': tracksStore.tracks.length > 0,
+          'inset-0 justify-center items-center': tracksStore.tracks.length === 0,
+        }"
+        :type="'default'"
+      />
+    </Transition>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.1s ease-in-out;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+</style>
