4b68183a92d3c38877855b1de65a5819afb6acd4
sayranovv <sayranoveldar@gmail.com>
2025-06-22 13:56:57 +0500

    Fix middleware

--- frontend/middleware/auth.global.ts
+++ frontend/middleware/auth.global.ts
@@ -1,10 +1,9 @@
 import { useAuthStore } from '~/stores/auth'
-import { process } from 'std-env'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const authStore = useAuthStore()
   await authStore.checkSession()
-  if (!authStore.user && process.client && to.path !== '/login') {
+  if (!authStore.user && to.path !== '/login') {
     console.log('auth middleware:', authStore.user)
     return navigateTo('/login')
   }
